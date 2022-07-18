71d85a58b157f18e016519bda8244df667562658
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-07-18 22:24:37 +0530

    Create ColumnWiseSum.cpp

---
+++ Coding Ninjas/C++/lect_10_characterArray&2Darrays/ColumnWiseSum.cpp
@@ -0,0 +1,49 @@
+/*
+
+Column Wise Sum
+Send Feedback
+Given a 2D integer array of size M*N, find and print the sum of ith column elements separated by space.
+Input Format :
+First and only line of input contains M and N, followed by M * N space separated integers representing the elements in the 2D array.
+Output Format :
+Sum of every ith column elements (separated by space)
+Constraints :
+1 <= M, N <= 10^3
+Sample Input :
+4 2 1 2 3 4 5 6 7 8
+Sample Output :
+16 20
+
+*/
+
+// solution 
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    	int m,n,sum=0;;
+		int arr[1000][1000];
+    cin>>m>>n;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+  
+    for(int i=0;i<n;i++)
+    {
+        sum=0;
+        for(int j=0;j<m;j++)
+        {
+            sum=sum+arr[j][i];
+        }
+        cout<<sum<<' ';
+        
+    }
+}
+
