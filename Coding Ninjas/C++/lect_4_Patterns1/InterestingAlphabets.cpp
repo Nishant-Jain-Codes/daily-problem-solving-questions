bf9543285f64c2b93cf27adee33216f89b2816a8
nishant jain <83751330+nishant-d20@users.noreply.github.com>
2022-07-18 15:57:40 +0530

    Create InterestingAlphabets.cpp

---
+++ Coding Ninjas/C++/lect_4_Patterns1/InterestingAlphabets.cpp
@@ -0,0 +1,65 @@
+/*
+
+Code : Interesting Alphabets
+Send Feedback
+Print the following pattern for the given number of rows.
+Pattern for N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+Input format :
+N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+8
+Sample Output 1:
+H
+GH
+FGH
+EFGH
+DEFGH
+CDEFGH
+BCDEFGH
+ABCDEFGH
+Sample Input 2:
+7
+Sample Output 2:
+G
+FG
+EFG
+DEFG
+CDEFG
+BCDEFG
+ABCDEFG
+
+*/
+
+// solution 
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    
+    int n;
+ cin>>n;
+ for(int i = 1;i<=n;i++)
+ {
+   char c='A'+n-i;
+   for(int a = 1;a<=i;a++)
+   {
+     cout<<c;
+     c++;
+   }
+   cout<<endl;
+ }
+
+  return 0;
+    
+}
