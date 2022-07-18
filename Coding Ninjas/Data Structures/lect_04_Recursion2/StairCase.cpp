7126a8983c1b68ed8851688aae8051be65e7d767
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-07-18 21:55:28 +0530

    Create StairCase.cpp

---
+++ Coding Ninjas/Data Structures/lect_04_Recursion2/StairCase.cpp
@@ -0,0 +1,42 @@
+/*
+
+Staircase
+Send Feedback
+A child is running up a staircase with N steps, and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method to count how many possible ways the child can run up to the stairs. You need to return number of possible ways W.
+Input format :
+Integer N
+Output Format :
+Integer W
+Constraints :
+1 <= N <= 30
+Sample Input 1 :
+4
+Sample Output 1 :
+7
+Sample Input 2 :
+5
+Sample Output 2 :
+13
+
+*/
+
+// solution 
+
+int staircase(int n){
+   if(n==0)
+   {
+       return 1;
+   }
+   else if(n==1)
+   {
+       return 1;
+   }
+   else if(n==2)
+   {
+       return 2;
+   }
+    int a=staircase(n-1);
+    int b=staircase(n-2);
+    int c=staircase(n-3);
+    return a+b+c;
+}
