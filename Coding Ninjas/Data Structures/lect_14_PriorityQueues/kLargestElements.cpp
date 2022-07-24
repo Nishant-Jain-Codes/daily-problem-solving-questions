0353f4e9ba0ad8cf9cc8da845e8e10ce7f44d459
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-07-24 11:37:17 +0530

    Create kLargestElements.cpp

---
+++ Coding Ninjas/Data Structures/lect_14_PriorityQueues/kLargestElements.cpp
@@ -0,0 +1,56 @@
+/*
+
+Code : K largest elements
+Send Feedback
+You are given with an integer k and an array of integers that contain numbers in random order. Write a program to find k largest numbers from given array. You need to save them in an array and return it.
+Time complexity should be O(nlogk) and space complexity should be not more than O(k).
+Order of elements in the output is not important.
+Input Format :
+Line 1 : Size of array (n)
+Line 2 : Array elements (separated by space)
+Line 3 : Integer k
+Output Format :
+k largest elements
+Sample Input :
+13
+2 12 9 16 10 5 3 20 25 11 1 8 6 
+4
+Sample Output :
+12
+16
+20
+25
+
+*/
+
+// solution 
+
+#include<queue>
+vector<int> kLargest(int arr[], int size, int k){
+
+    if(size==0)
+    {   vector<int>ans;
+        return ans;
+    }
+    priority_queue<int,vector<int>,greater<int>> help;
+      vector<int>ans;
+    for(int i =0;i<k;i++)
+    {
+        help.push(arr[i]);
+    }
+    for(int i = k;i<size;i++)
+    {
+       if(help.top()<arr[i])
+        {
+            help.pop();
+            help.push(arr[i]);
+        }
+    }
+    while(!help.empty())
+    {
+        ans.push_back(help.top());
+        help.pop();
+    }
+    return ans;
+
+}
