26f0d4e18ab511a5c59baa452dd557f3bd95390f
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-09-18 10:47:11 +0530

    Create CHOPRT.cpp

---
+++ Code Chef/learning/for Unrated coders/CHOPRT.cpp
@@ -0,0 +1,53 @@
+/*
+https://www.codechef.com/LP0TO101/problems/CHOPRT
+
+Problem
+Chef has just started Programming, he is in first year of Engineering. Chef is reading about Relational Operators.
+Relational Operators are operators which check relatioship between two values. Given two numerical values A and B you need to help chef in finding the relationship between them that is,
+
+First one is greater than second or, First one is less than second or, First and second one are equal.
+ 
+
+Input
+First line contains an integer T, which denotes the number of testcases. Each of the T lines contain two integers A and B.
+
+Output
+For each line of input produce one line of output. This line contains any one of the relational operators
+'<' , '>' , '='.
+
+Constraints
+1 ≤ T ≤ 10000 1 ≤ A, B ≤ 1000000001
+Sample 1:
+Input
+Output
+3
+10 20
+20 10
+10 10
+<
+>
+=
+Explanation:
+In this example 1 as 10 is lesser than 20
+*/
+
+// solution 
+
+void solution()
+{
+  int a,b;cin>>a>>b;
+  if(a>b)
+  {co('>');}
+  else if(a<b)
+  {
+      {co('<');}
+  }
+  else 
+  {
+      {co('=');}
+  }
+    
+  
+  
+  
+}
