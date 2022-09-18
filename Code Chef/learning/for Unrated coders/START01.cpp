fe24c24b4075455bfd138ce92f1ec67e782294f1
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-09-18 10:42:54 +0530

    Create START01.cpp

---
+++ Code Chef/learning/for Unrated coders/START01.cpp
@@ -0,0 +1,32 @@
+/*
+https://www.codechef.com/LP0TO101/problems/START01
+
+Problem
+Problem Statement
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+
+Constraints
+0 ≤ n ≤ 105
+Sample Input
+123
+Sample Output
+123
+*/
+
+// solution 
+
+int32_t main()
+{
+	// your code goes here
+	int t;
+	cin>>t;
+	co(t);
+	
+	return 0;
+}
