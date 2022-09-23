3d666eac5cf4711190d7e16a94f15d623fc24e18
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-09-23 21:00:41 +0530

    Create sept_LunchTime_div3_FLIPCARDS.cpp

---
+++ Code Chef/Contests/sept_LunchTime_div3_FLIPCARDS.cpp
@@ -0,0 +1,24 @@
+/*
+https://www.codechef.com/LTIME112C/problems/FLIPCARDS
+
+*/
+
+// solution 
+
+
+// CODE STARTS HERE-------------------------------------------------------------------------------------------
+// space complexity : O(1)
+// time complexity :  O(1)
+void solution()
+{
+  int n;cin>>n;
+  int x;cin>>x;
+  int y=n-x;
+  int ans=min(x,y);
+  co(ans);
+    
+  
+  
+  
+}
+//CODE ENDS HERE----------------------------------------------------------------------------------------------
