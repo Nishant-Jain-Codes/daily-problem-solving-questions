f6cbb820d47311c9b0eafd42abf441e99fd64a4b
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-09-23 21:02:09 +0530

    Create sept_LunchTime_div3_STICKBREAK.cpp

---
+++ Code Chef/Contests/sept_LunchTime_div3_STICKBREAK.cpp
@@ -0,0 +1,29 @@
+/*
+
+https://www.codechef.com/LTIME112C/problems/STICKBREAK
+
+*/
+
+// solution 
+
+
+// CODE STARTS HERE-------------------------------------------------------------------------------------------
+// space complexity : O(1)
+// time complexity :  O(1)
+void solution()
+{
+    int n;cin>>n;
+    int k;cin>>k;
+     
+  if(n%k!=0)
+  {
+      co(1);
+  }
+  else 
+  {
+      co(0);
+  }
+  
+  
+}
+//CODE ENDS HERE----------------------------------------------------------------------------------------------
