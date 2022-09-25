7e7b85c356f27b799445bad43136667dcfc0d654
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-09-25 10:48:53 +0530

    Create CHFNA.cpp

---
+++ Code Chef/learning/2-star coders/Bit manipulation/CHFNA.cpp
@@ -0,0 +1,24 @@
+/*
+
+https://www.codechef.com/LP2TO305/problems/CHFNA?tab=statement
+
+*/
+
+// solution
+
+
+
+// CODE STARTS HERE-------------------------------------------------------------------------------------------
+// space complexity : O(1)
+// time complexity :  O(1)
+void solution()
+{
+  
+    int x,y;
+    cin>>x>>y;
+    int ans=(x^y);
+    co(ans);
+  
+  
+}
+//CODE ENDS HERE----------------------------------------------------------------------------------------------
