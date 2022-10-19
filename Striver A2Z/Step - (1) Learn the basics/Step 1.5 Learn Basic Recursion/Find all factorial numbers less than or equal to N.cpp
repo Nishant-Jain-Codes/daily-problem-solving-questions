4862064578a00c03a6aad445ab6278f733d2afd6
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-10-19 22:39:59 +0530

    another one

---
+++ Striver A2Z/Step - (1) Learn the basics/Step 1.5 Learn Basic Recursion/Find all factorial numbers less than or equal to N.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<long long> factorialNumbers(long long N)
+    {
+        // Write Your Code here
+        vector<long long> ans;
+        ;long long j=1;
+        for(long long i =1;i<=N;i*=j)
+        {
+            ans.push_back(i);
+            j++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+        cin>>N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for(auto num : ans){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
