77adb8baf3a615dc4df7ad7fc8c3749544a0b713
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-10-20 19:49:01 +0530

    another one

---
+++ Striver A2Z/Step - (3) Solve Problems on Arrays/Step 3.1 Easy/26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,20 @@
+/*
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        while(i<nums.size()-1)
+        {
+            if(nums[i]==nums[i+1])
+            {   
+                nums.erase(nums.begin()+i);
+            }
+            else 
+                i++;
+        }
+        return nums.size();
+    }
+};
