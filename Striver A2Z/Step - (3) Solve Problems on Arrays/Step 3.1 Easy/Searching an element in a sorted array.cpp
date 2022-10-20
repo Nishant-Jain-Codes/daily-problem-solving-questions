1476582192a5ec063293a554e6c8b93adfb50365
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-10-20 19:54:33 +0530

    one more

---
+++ Striver A2Z/Step - (3) Solve Problems on Arrays/Step 3.1 Easy/Searching an element in a sorted array.cpp
@@ -0,0 +1,61 @@
+/*
+
+https://practice.geeksforgeeks.org/problems/who-will-win-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=who-will-win
+
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+    
+       // Your code here
+       for(int i=0;i<N;i++)
+       {
+           if(arr[i]==K)
+            return true;
+            
+           if(arr[i]>K)
+            return -1;
+       }
+       return -1;
+       
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.searchInSorted(arr, n, k) << endl;
+
+    }
+
+	return 0; 
+} 
+
+// } Driver Code Ends
