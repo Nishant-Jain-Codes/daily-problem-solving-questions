efb8b446f71c40f37dd4eb28e85172fbd0d45fd8
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-10-23 22:12:52 +0530

    siuuu

---
+++ Striver A2Z/Step - (1) Learn the basics/Step 1.5 Learn Basic Recursion/Palindrome String.cpp
@@ -0,0 +1,51 @@
+/*
+
+https://practice.geeksforgeeks.org/problems/palindrome-string0817/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=palindrome-string
+
+*/
+
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string s)
+	{
+	    // Your code goes here
+	    string s2 = s;
+	    reverse(s2.begin(),s2.end());
+	    return s2==s;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
