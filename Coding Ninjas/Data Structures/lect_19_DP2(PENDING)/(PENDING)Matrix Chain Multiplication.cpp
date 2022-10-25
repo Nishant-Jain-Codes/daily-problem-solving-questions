2aa61cd754a2df17e2fa264c8866cd471056ab4e
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-10-25 08:26:02 +0530

    solved 1 dp problem

--- Coding Ninjas/Data Structures/lect_19_DP2(PENDING)/(PENDING)Matrix Chain Multiplication.cpp
+++ Coding Ninjas/Data Structures/lect_19_DP2(PENDING)/(PENDING)Matrix Chain Multiplication.cpp
@@ -19,3 +19,67 @@ If we multiply in order- A1*(A2*A3), then number of multiplications required are
 If we multiply in order- (A1*A2)*A3, then number of multiplications required are 8000.
 Thus minimum number of multiplications required are 8000. 
 */
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+int multAns(int *arr,int si,int ei,int **dp){
+
+    if(si==ei||si==ei-1)
+        return 0;
+    int ans=INT_MAX;
+    for(int k=si+1;k<ei;k++)
+    {   int m1,m2,m3;
+        if(dp[si][k]!=-1)
+            m1=dp[si][k];
+        else 
+        {
+            m1=multAns(arr,si,k,dp);
+            dp[si][k]=m1;
+        }
+        if(dp[k][ei]!=-1)
+            m2=dp[k][ei];
+        else 
+        {
+            m2=multAns(arr,k,ei,dp);
+            dp[k][ei]=m2;
+        }
+        m3 = arr[si]*arr[k]*arr[ei];
+        int sc=m1+m2+m3;
+        ans=min(ans,sc);
+    }    
+    return ans;
+}
+int matrixChainMultiplication(int* arr, int n) {
+    // Write your code here
+    int **dp= new int* [n+1];
+    for(int i=0;i<=n;i++)
+    {
+        dp[i]=new int[n+1];
+        for(int j=0;j<=n;j++)
+        {
+            dp[i][j]=-1;
+        }
+    }
+    int ans=multAns(arr,0,n,dp);
+    for(int i=0;i<=n;i++)
+        delete [] dp[i];
+    delete []dp;
+    return ans;
+}
+int main() {
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+
+    for (int i = 0; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << matrixChainMultiplication(arr, n);
+
+    delete[] arr;
+}
