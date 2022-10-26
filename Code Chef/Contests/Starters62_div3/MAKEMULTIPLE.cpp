a90f306a25f46db9b1fbae0c2332bd9b0a58de17
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2022-10-26 22:10:04 +0530

    Create MAKEMULTIPLE.cpp

---
+++ Code Chef/Contests/Starters62_div3/MAKEMULTIPLE.cpp
@@ -0,0 +1,59 @@
+/*
+
+https://www.codechef.com/START62C/problems/MAKEMULTIPLE
+
+*/
+
+
+// linkers , defines , constants
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int             long long int
+#define lb             lower_bound
+#define in(v,n)        for(int i=0;i<n;i++) cin>>v[i];
+#define co(x)         cout<<x<<endl;
+
+const string yes = "YES";
+const string no = "NO";
+void solution();
+void YorN(bool ans)
+{
+    if(ans)
+        {
+            co(yes);
+            
+        }
+    else 
+        {
+            co(no);
+        }
+}
+
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solution();
+     
+    }
+    return 0;
+}
+//CODE STARTS HERE-----------------------------------------------------------
+
+// time complexity O(1)
+// space complexity O(1)
+
+void solution()
+{
+    int x,y;
+    cin>>x>>y;
+    bool ans = false;
+    if(x<=(y/2)||x==y)
+    ans= true;
+    YorN(ans);
+}
+
+//CODE ENDS HERE----------------------------------------------------------------------------------------------
