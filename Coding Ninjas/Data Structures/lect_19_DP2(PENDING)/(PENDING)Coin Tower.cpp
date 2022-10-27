dd8bfc336ec993c42eaa786f8e5f84f67acdfc8b
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-10-27 12:33:47 +0530

    solved a dp problem

--- Coding Ninjas/Data Structures/lect_19_DP2(PENDING)/(PENDING)Coin Tower.cpp
+++ Coding Ninjas/Data Structures/lect_19_DP2(PENDING)/(PENDING)Coin Tower.cpp
@@ -21,3 +21,35 @@ Explanation to Sample Input 1:
 Initially, there are 4 coins,  In the first move, Beerus can remove either 1, 2, or 3 coins in all three cases, Whis can win by removing all the remaining coins. 
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+string findWinner(int n, int x, int y)
+{
+    // Write your code here .
+    int *arr = new int[n+1];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        int a=0,b=0,c=0;
+        a=arr[i-1]^1;
+        if(i-x>=0)
+            b=arr[i-x]^1;
+        if(i-y>=0)
+            c=arr[i-y]^1;
+        arr[i]=max(a,max(b,c));
+    }
+
+    if(arr[n]==1)
+        return "Beerus";
+    else 
+        return "Whis";
+}
+int main()
+{
+    int n, x, y;
+    cin >> n >> x >> y;
+    cout << findWinner(n, x, y);
+}
