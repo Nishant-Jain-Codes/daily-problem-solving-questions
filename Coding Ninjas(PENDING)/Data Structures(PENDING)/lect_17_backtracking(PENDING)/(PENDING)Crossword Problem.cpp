a2593249f5f158e45bc69b26fdae96b6cbf27ada
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-11-30 10:05:00 +0530

    solving crossword problem

--- Coding Ninjas(PENDING)/Data Structures(PENDING)/lect_17_backtracking(PENDING)/(PENDING)Crossword Problem.cpp
+++ Coding Ninjas(PENDING)/Data Structures(PENDING)/lect_17_backtracking(PENDING)/(PENDING)Crossword Problem.cpp
@@ -63,3 +63,100 @@ CALIFORNIA
 
 //solution 
 
+#include<bits/stdc++.h>
+using namespace std;
+void printCrossWord(char **crossWord)
+{
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            cout<<crossWord[i][j];
+        }
+        cout<<endl;
+    }
+}
+bool possibleHorizontally(char **crossWord,string word,int i,int j)
+{
+    if(j-1>=0&&crossWord[i][j-1]!='+')
+        return false;
+    else if(j+word.length()<10&&arr[i][j+word.length()]!='+')
+        return false;
+    for(int jj=0;jj<word.length();jj++)
+    {
+        if(crossWord[i][j+jj]=='-'||crossWord[i][j+jj]==word[jj])
+            continue;
+        else 
+            return false;
+    }
+    return true;
+}
+bool possibleVertically(char **crossWord,string word,int i,int j)
+{
+    if(i-1>=0&&crossWord[i-1][j]!='+')
+        return false;
+    else if(i+word.length()<10&&arr[i+word.length()][j]!='+')
+        return false;
+    for(int ii=0;ii<word.length();ii++)
+    {
+        if(crossWord[i+ii][j]=='-'||crossWord[i+ii][j]==word[ii])
+            continue;
+        else 
+            return false;
+    }
+    return true;
+}
+void solver(char **crossWord ,vector<string> words,int idx)
+{   
+    if(idx==words.size())
+    {
+        printCrossWord(crossWord);
+        return;
+    }
+    string w = words[idx];
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            if(crossWord[i][j]=='-'||crossWord[i][j]==w[0])
+            {
+                if(possibleHorizontally(crossWord,w,i,j))
+                {
+                    placeHorizontally(crossWord,w,i,j);
+                    solution(crossWord,words,idx+1);
+                    unPlaceHorizontally(crossWord,w,i,j)
+                }
+            }
+        }
+    }
+}
+int main(){
+    
+    // write your code here
+    char crossWord[10][10];
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            cin>>crossWord[i][j];
+        }
+    }
+    string in; cin>>in;
+    vector<string> words;
+    string w="";
+    for(int i=0;i<in.length();i++)
+    {
+        if(in[i]==';')
+        {
+            words.push_back(w);
+            w="";
+        }
+        else
+        {
+            w.push_back(in[i]);
+        }
+    }
+    words.push_back(w);
+    solver(crossWord,words,0);
+    return 0;
+}
