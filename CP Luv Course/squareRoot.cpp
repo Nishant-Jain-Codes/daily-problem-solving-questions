1a3757bc12cd272583f5870413171036b53ffe19
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-12-02 13:42:08 +0530

    ,

---
+++ CP Luv Course/squareRoot.cpp
@@ -0,0 +1,74 @@
+// linkers , defines , constants
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int             long long int
+#define in(v,n)        for(int i=0;i<n;i++) cin>>v[i];
+#define co(x)         cout<<x<<endl;
+const string yes = "YES";
+const string no = "NO";
+void solution();
+//===============SHORTCUT FUNCTIONS===================
+void YorN(bool ans)
+{
+    if(ans)
+        {
+            co(yes);
+            
+        }
+    else 
+        {
+            co(no);
+        }
+}
+void inVec(vector<int>&v,int &n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int in;cin>>in;
+        v.push_back(in);
+    }
+    return;
+}
+//=============MAIN FUNCTION===============
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+    // your code goes here
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solution();
+     
+    }
+    return 0;
+}
+
+
+
+
+// CODE STARTS HERE-------------------------------------------------------------------------------------------
+// time complexity :  O(n)
+// space complexity : O(n)
+double eps = 1e-5;
+void solution()
+{
+    double n;cin>>n;
+    double lo=1,hi = n,mid;
+    while(hi-lo > eps)
+    {
+         mid = (hi+lo)/2;
+        if(mid*mid<n)
+            lo=mid;
+        else 
+            hi=mid;
+    }
+    co(lo);
+
+}
+
+//CODE ENDS HERE----------------------------------------------------------------------------------------------
