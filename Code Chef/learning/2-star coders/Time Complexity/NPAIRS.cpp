55891a4c4b267707eef75ce6a8740a719c35c247
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-12-06 06:00:41 +0530

    cp pactice

---
+++ Code Chef/learning/2-star coders/Time Complexity/NPAIRS.cpp
@@ -0,0 +1,77 @@
+// https://www.codechef.com/LP2TO308/problems/NPAIRS
+
+// linkers , defines , constants
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int             long long int
+#define in(v,n)        for(int i=0;i<n;i++) cin>>v[i];
+#define co(x)         cout<<x<<endl;
+const string yes = "YES";
+const string no = "NO";
+void solution();
+//===============SHORTCUT FUNCTIONS===================
+void YorN(bool ans)
+{
+    if(ans)
+        {
+            co(yes);
+            
+        }
+    else 
+        {
+            co(no);
+        }
+}
+void inVec(vector<int>&v,int &n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int in;cin>>in;
+        v.push_back(in);
+    }
+    return;
+}
+//=============MAIN FUNCTION===============
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+    // your code goes here
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solution();
+     
+    }
+    return 0;
+}
+
+
+
+
+// CODE STARTS HERE-------------------------------------------------------------------------------------------
+// time complexity :  O(n)
+// space complexity : O(n)
+const int N = 1e5+10;
+int  arr[N];
+void solution()
+{
+  int n;cin>>n;
+  string s;cin>>s;
+  int ans=0;
+  for(int i=0;i<n;i++)
+  {
+    for(int j=i+1;j<=min(i+9,n);j++)
+    {
+        if((j-i)==abs(s[j]-s[i]))
+            ans++;
+    }
+  }
+  co(ans);
+}
+
+//CODE ENDS HERE----------------------------------------------------------------------------------------------
