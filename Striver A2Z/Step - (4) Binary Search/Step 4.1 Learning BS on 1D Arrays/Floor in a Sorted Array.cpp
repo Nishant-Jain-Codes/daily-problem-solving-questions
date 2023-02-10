fc3697e8e0b0e4425baa3b3ef11f2ee628415eee
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-02-10 18:28:21 +0530

    solved a strive problem

---
+++ Striver A2Z/Step - (4) Binary Search/Step 4.1 Learning BS on 1D Arrays/Floor in a Sorted Array.cpp
@@ -0,0 +1,62 @@
+// https://practice.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%3BbatchId=154&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=floor-in-a-sorted-array
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+        int ans = -1;
+        int si=0,ei=n-1,mid;
+        while(si<=ei){
+            mid = si + ((ei-si)/2);
+            if(v[mid]>x){
+                ei=mid-1;
+            }else if(v[mid]<x){
+                ans = mid;
+                si=mid+1;
+            }
+            else 
+                return mid;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    long long x;
+	    cin >> x;
+	    
+	    vector<long long> v;
+	    
+	    for(long long i = 0;i<n;i++){
+	        long long temp;
+	        cin >> temp;
+	        v.push_back(temp);
+	    }
+	    Solution obj;
+	    cout << obj.findFloor(v, n, x) << endl;
+	   
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
