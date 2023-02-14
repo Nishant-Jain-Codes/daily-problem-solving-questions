8c1b5a0e1123a3335e7c3c2caae03565cbcd3ddb
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-02-14 21:59:08 +0530

    started 6.3 step of a2z

---
+++ Striver A2Z/Step - (6) Learn LinkedList/Step 6.3  Medium Problems of LL/876. Middle of the Linked List.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head==NULL)
+            return head;
+        ListNode * fast = head;
+        ListNode * slow = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            fast = fast->next->next;
+            slow=slow->next;
+        }
+        if(fast->next!=NULL)
+            slow=slow->next;
+        return slow;
+    }
+};
