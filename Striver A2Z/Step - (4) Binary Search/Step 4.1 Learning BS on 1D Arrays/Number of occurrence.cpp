499e0ed87e6163a56045fcbace3e61a3bc29d9d1
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-02-17 13:09:46 +0530

    another one

---
+++ Striver A2Z/Step - (4) Binary Search/Step 4.1 Learning BS on 1D Arrays/Number of occurrence.cpp
@@ -0,0 +1,47 @@
+// https://practice.geeksforgeeks.org/problems/number-of-occurrence2259/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=number-of-occurrence
+
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int lb = lower_bound(arr,arr+n,x)-arr;
+	    int ub = upper_bound(arr,arr+n,x)-arr;
+	    if(lb!=n&&arr[lb]==x)
+	        return ub-lb;
+	    else 
+	        return 0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
