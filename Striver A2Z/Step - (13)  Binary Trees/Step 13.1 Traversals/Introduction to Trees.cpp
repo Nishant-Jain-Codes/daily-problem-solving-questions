124ad0927fb21124e95c2e1db294532d2583ec9c
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-04-14 13:32:53 +0530

    solving a2z

---
+++ Striver A2Z/Step - (13)  Binary Trees/Step 13.1 Traversals/Introduction to Trees.cpp
@@ -0,0 +1,41 @@
+// https://practice.geeksforgeeks.org/problems/introduction-to-trees/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=introduction-to-trees
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countNodes(int i) {
+        // your code here
+        if(i==0)
+            return 0;
+        return pow(2,i-1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int i;
+        cin >> i;
+        Solution ob;
+        int res = ob.countNodes(i);
+
+        cout << res;
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
