fb5f64c12355845c90752636ffd9e55bbc02c697
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-04-20 20:36:58 +0530

    solving A2z

---
+++ Striver A2Z/Step - (5) Strings E&M/Step 5.1  Basic and Easy String Problems/796. Rotate String.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/rotate-string/description/
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        return s.length()==goal.length()&&(s+s).find(goal)!=string::npos;
+    }
+};
---
+++ Striver A2Z/Step - (5) Strings E&M/Step 5.1  Basic and Easy String Problems/Check if strings are rotations of each other or not.cpp
@@ -0,0 +1,40 @@
+// https://practice.geeksforgeeks.org/problems/check-if-strings-are-rotations-of-each-other-or-not-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=check-if-strings-are-rotations-of-each-other-or-not
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s,string goal)
+    {
+        // Your code here
+         return s.length()==goal.length()&&(s+s).find(goal)!=string::npos;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
