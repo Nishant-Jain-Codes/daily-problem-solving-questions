f455ebe3bc197b02dceeb0be807b476455051a3c
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-05-03 20:48:51 +0530

    solve a2z problem

---
+++ Striver A2Z/Step - (13)  Binary Trees/Step 13.3  Hard Problems/Unique Binary Tree Requirements.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    bool isPossible(int a,int b)
+    {
+        //code here
+        return (a==2 || b==2) && (a==1 || a==3||b==1||b==3);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b; 
+        cin>>a>>b;
+        Solution obj;
+        bool ans=obj.isPossible(a,b);
+        cout<<ans<<"\n";
+    }
+}
+// } Driver Code Ends
