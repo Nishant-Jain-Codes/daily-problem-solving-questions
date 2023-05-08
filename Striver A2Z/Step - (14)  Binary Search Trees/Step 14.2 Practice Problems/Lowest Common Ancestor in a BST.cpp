9c681f0ed8aad2fa4c6bf53b121035335688c2b3
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-05-08 20:06:27 +0530

    solve BST problem

---
+++ Striver A2Z/Step - (14)  Binary Search Trees/Step 14.2 Practice Problems/Lowest Common Ancestor in a BST.cpp
@@ -0,0 +1,119 @@
+// https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-bst/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=lowest-common-ancestor
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+Node* LCA(Node * root , int l , int h);
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        int l , h;
+		getline(cin,s);
+		scanf("%d ",&l);
+		scanf("%d ",&h);
+        Node* root = buildTree(s);
+        cout<<LCA(root ,  l ,  h)->data<<endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
+
+
+//Function to find the lowest common ancestor in a BST. 
+Node* LCA(Node *root, int p, int q)
+{
+   //Your code here
+if(!root)   
+    return NULL;
+if(p>q)
+    return  LCA(root,q,p);
+if(root->data>q)
+    return  LCA(root->left,p,q);
+else if(root->data<p)
+    return  LCA(root->right,p,q);
+else 
+    return root;
+}
+
+
