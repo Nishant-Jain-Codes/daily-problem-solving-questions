258779c1d02d1fdb73e4e2d974862890265876ae
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-05-29 22:37:41 +0530

    started step 15.5

---
+++ Striver A2Z/Step - (15) Graphs/Step 15.5  MinimumSpanningTree,Disjoint Set and Problems/Minimum Spanning Tree.cpp
@@ -0,0 +1,69 @@
+// https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        int sum=0;
+        vector<bool> visited(V,false);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,0});
+        while(!pq.empty()){
+            int curNode = pq.top().second;
+            int curWeight = pq.top().first;
+            pq.pop();
+            if(visited[curNode]) continue;
+            visited[curNode] = true;
+            sum+=curWeight;
+            for(auto it:adj[curNode]){
+                if(!visited[it[0]]){
+                    pq.push({it[1],it[0]});
+                }
+            }
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
