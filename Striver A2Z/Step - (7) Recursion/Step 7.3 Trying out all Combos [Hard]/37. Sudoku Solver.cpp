0cec5a1e1a9fff016a009c28b3f8f7278736e977
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-06-16 18:38:45 +0530

    sudoku solver

---
+++ Striver A2Z/Step - (7) Recursion/Step 7.3 Trying out all Combos [Hard]/37. Sudoku Solver.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private: 
+    bool isValid(vector<vector<char>>& board,int row , int col , char c){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c){
+                return false;
+            }
+            if(board[row][i]==c){
+                return false;
+            }
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool sudokuSolver(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';c++){
+                        if(isValid(board,i,j,c)){
+                            board[i][j]=c;
+                            if(sudokuSolver(board)){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        bool ans = sudokuSolver(board);
+    }
+};
