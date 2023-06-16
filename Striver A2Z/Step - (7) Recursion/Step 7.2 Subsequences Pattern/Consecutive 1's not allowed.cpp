aa1215d05aa29d8b2a4a728feb859c3bfe53e516
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-06-16 19:06:05 +0530

    added dp solution to this problem

--- Striver A2Z/Step - (7) Recursion/Step 7.2 Subsequences Pattern/Consecutive 1's not allowed.cpp
+++ Striver A2Z/Step - (7) Recursion/Step 7.2 Subsequences Pattern/Consecutive 1's not allowed.cpp
@@ -22,13 +22,26 @@ private:
             return helper(n-1,'0');
         }
     }
+    ll tabulation(int n ){
+        vector<vector<int>> dp(n + 1,vector<int>(2, 0));
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+            dp[i][1] = dp[i - 1][0];
+        }
+
+        return (dp[n-1][0] + dp[n-1][1]) % mod;
+    }
 public:
 	// #define ll long long
 	ll countStrings(int n) {
 	    // code here
-        return helper(n,' ');
+        return tabulation(n);
 	}
 };
+
 
 //{ Driver Code Starts.
 
