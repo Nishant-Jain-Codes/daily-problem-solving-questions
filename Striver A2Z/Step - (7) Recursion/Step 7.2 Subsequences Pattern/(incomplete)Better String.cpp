1a8ebba427a3d7494f7d6b03246a133e26bdc2c9
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-06-17 10:50:26 +0530

    trying out a different approach

---
+++ Striver A2Z/Step - (7) Recursion/Step 7.2 Subsequences Pattern/(incomplete)Better String.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+private:
+    int memoisation(const string & s , int idx , vector<int> & dp){
+        if(idx==s.size()){
+            return 0;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int ans = 1;
+        for(int i=idx ; i<s.size();i++){
+            if(i!=idx && s[i]==s[i-1]){
+                continue;
+            }
+            ans += memoisation(s,i+1,dp);
+        }
+        return dp[idx] = ans;
+    }
+
+public:
+    string betterString(string s1, string s2) {
+        // code here
+        string str1 = s1;
+        string str2 = s2;
+        sort(str1.begin(),str1.end());
+        sort(str2.begin(),str2.end());
+        vector<int> dp1 (str1.size(),-1);
+        vector<int> dp2 (str2.size(),-1);
+        int sol1 = memoisation(str1,0,dp1);
+        int sol2 = memoisation(str2,0,dp2);
+        //cout<<sol1<<" "<<sol2<<endl;
+        if(sol1>=sol2){
+            return s1;
+        }
+        return s2;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str1, str2;
+        cin >> str1 >> str2;
+        Solution obj;
+        string ans = obj.betterString(str1, str2);
+        cout << ans << "\n";
+    }
+}
+
+// } Driver Code Ends
