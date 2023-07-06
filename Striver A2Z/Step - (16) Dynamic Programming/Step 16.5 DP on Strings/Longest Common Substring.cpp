2df9a6825096ad674873dcaf87825ecf20ead889
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-07-07 00:35:03 +0530

    solved a dp problem

--- Striver A2Z/Step - (16) Dynamic Programming/Step 16.5 DP on Strings/Longest Common Substring.cpp
+++ Striver A2Z/Step - (16) Dynamic Programming/Step 16.5 DP on Strings/Longest Common Substring.cpp
@@ -1,14 +1,56 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // } Driver Code Ends
-class Solution{
-    public:
-    
-    int longestCommonSubstr (string S1, string S2, int n, int m)
+class Solution
+{
+public:
+    int longestCommonSubstr(string S1, string S2, int n, int m)
     {
         // your code here
+        return optimized(S1, S2, n, m);
+        return tabulation(S1, S2, n, m);
+    }
+
+private:
+    int tabulation(string S1, string S2, int n, int m)
+    {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+                if (S1[i - 1] == S2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = max(ans, dp[i][j]);
+                }
+                else
+                    dp[i][j] = 0;
+        return ans;
+    }
+    int optimized(string S1, string S2, int n, int m)
+    {
+        vector<int> prev(max(n, m) + 1, 0);
+        vector<int> curr(max(n, m) + 1, 0);
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (S1[i - 1] == S2[j - 1])
+                {
+                    curr[j] = 1 + prev[j - 1];
+                    ans = max(ans, curr[j]);
+                }
+                else
+                {
+                    curr[j] = 0;
+                }
+            }
+            prev = curr;
+        }
+        return ans;
     }
 };
 
@@ -16,15 +58,17 @@ class Solution{
 
 int main()
 {
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--)
     {
-        int n, m; cin >> n >> m;
+        int n, m;
+        cin >> n >> m;
         string s1, s2;
         cin >> s1 >> s2;
         Solution ob;
 
-        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+        cout << ob.longestCommonSubstr(s1, s2, n, m) << endl;
     }
 }
 // Contributed By: Pranay Bansal
