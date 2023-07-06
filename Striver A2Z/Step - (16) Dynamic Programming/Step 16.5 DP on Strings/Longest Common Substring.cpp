d932c94a2b81ed8cc249d59ff660697548dd2b79
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-07-06 22:34:26 +0530

    more dp problems to solve

---
+++ Striver A2Z/Step - (16) Dynamic Programming/Step 16.5 DP on Strings/Longest Common Substring.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+        // your code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
