ff2706d09e27f4f53c5d22ef74493526e6799feb
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-07-20 20:30:25 +0530

    another one !

---
+++ Striver A2Z/Step - (8)  Bit Manipulation/Step 8.2 Interview Problems/Bit Difference.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int start, int goal){
+        
+                return __builtin_popcount(start ^ goal);
+
+        // Your logic here
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;// input the testcases
+	while(t--) //while testcases exist
+	{
+		int a,b;
+		cin>>a>>b; //input a and b
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
