90ae7646f600da5df079852f87c3c7ec953b20c1
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-08-29 14:12:10 +0530

    added O(1) solution to this problems

--- Striver A2Z/Step - (9) Stack and Queues/Step 9.1 DONE Learning/155. Min Stack.cpp
+++ Striver A2Z/Step - (9) Stack and Queues/Step 9.1 DONE Learning/155. Min Stack.cpp
@@ -1,5 +1,57 @@
 https://leetcode.com/problems/min-stack/description/
+//O(1) space
+class MinStack {
+    stack<long long int> s;
+    long long int minEle;
+    
+public:
+    MinStack() {
+        
+    }
+    
+    void push(long long int val) {
+        if (s.empty()) {
+            s.push(val);
+            minEle = val;
+        } else if (val <= minEle) {
+            s.push(2 * val - minEle);
+            minEle = val;
+        } else {
+            s.push(val);
+        }
+    }
+    
+    void pop() {
+        if (s.empty())
+            return;
+        
+        long long int topVal = s.top();
+        s.pop();
+        
+        if (topVal < minEle) {
+            minEle = 2 * minEle - topVal;
+        }
+    }
+    
+    int top() {
+        if (s.empty())
+            return -1;
+        
+        long long int topVal = s.top();
+        if (topVal < minEle) {
+            return minEle;
+        }
+        return topVal;
+    }
+    
+    int getMin() {
+        if (s.empty())
+            return -1;
+        return minEle;
+    }
+};
 
+//O(n) space
 class MinStack {
     stack<pair<int,int>> stk ; //<top,curMin>
 public:
